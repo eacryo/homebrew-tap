e77490a4f1e5e40a8128f12c3f460b42cc1fc373
eacryo <eacryo@gmail.com>
2026-07-28 20:50:00 +0800

    correct sha256

--- Casks/oh-my-tab.rb
+++ Casks/oh-my-tab.rb
@@ -2,7 +2,7 @@ cask "oh-my-tab" do
   depends_on macos: :ventura
   depends_on arch: :arm64
   version "0.1.1"
-  sha256 "sha256:14c1ec3b580c989b006d19055c151c35f4645bebe0e26c3b34340660edf42171"
+  sha256 "d62c3bb07b87e477074b65513f461ead7bc30b18493dcaa608c7402c494d0f39"
   url "https://github.com/eacryo/oh-my-tab/releases/download/v#{version}/Oh-My-Tab.dmg"
   name "Oh-My-Tab"
   desc "macOS window switcher (Cmd+Tab alternative)"

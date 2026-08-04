afd0dbdb84aa5cc99d72f1ac9f8f26bd68b70f41
eacryo <eacryo@gmail.com>
2026-08-04 21:51:23 +0800

    update oh-my-tab.rb version to 0.1.4

--- Casks/oh-my-tab.rb
+++ Casks/oh-my-tab.rb
@@ -1,8 +1,8 @@
 cask "oh-my-tab" do
   depends_on macos: :ventura
   depends_on arch: :arm64
-  version "0.1.3"
-  sha256 "a2e5c8c540d539d0caf5077a0025324b12b63969c9ed7e901a36ece765db4a3e"
+  version "0.1.4"
+  sha256 "58f1bfbc8ecaa0ec35b48b353a4bb087131bf1dfb95739a71abe71e77eaaa425"
   url "https://github.com/eacryo/oh-my-tab/releases/download/v#{version}/Oh-My-Tab.dmg"
   name "Oh-My-Tab"
   desc "macOS window switcher (Cmd+Tab alternative)"
